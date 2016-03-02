e312777d32e298028c544b2cb44897ff397db839
Brandon Manke <brandonmanke@users.noreply.github.com>
2016-03-01 22:05:25 -0600

    Formatted script.js :star2:
    formatted script file to the standard of http://standardjs.com/

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
-//Name: Brandon Manke
-//Thanks to https://github.com/bnned
-//Web Scraper using node, cheerio, and phantom to scrape prnt.sc
-//Licenses: Cheerio (MIT), Phantom (ISC)
+// Name: Brandon Manke
+// Thanks to https://github.com/bnned
+// Web Scraper using node, cheerio, and phantom to scrape prnt.sc
+// Licenses: Cheerio (MIT), Phantom (ISC)
 
-var phantom = require('phantom');
-var cheerio = require("cheerio");
+var phantom = require('phantom')
+var cheerio = require('cheerio')
 
-function generateRandomURL() {
-  var url = "http://prnt.sc/";
-  var randomURL = "0123456789abcdefghijklmnopqrstuvwxyz";
+function generateRandomURL () {
+  var url = 'http://prnt.sc/'
+  var randomURL = '0123456789abcdefghijklmnopqrstuvwxyz'
 
   // from 0 to 5-6 characters in length
   for (var i = 0; i < Math.floor(Math.random() * (6 - 5 + 1)) + 5; i++) {
-    url += randomURL.charAt(Math.floor(Math.random() * randomURL.length));
+    url += randomURL.charAt(Math.floor(Math.random() * randomURL.length))
   }
-  return url;
+  return url
 }
 
-function filterURL(url) {
-  var filter = "8tdUI8N.png";
-  var imageShackFilter = "http://img"; //might need to specify this more or change it
+function filterURL (url) {
+  var filter = '8tdUI8N.png'
+  var imageShackFilter = 'http://img' // might need to specify this more or change it
 
-  //filters out images that are removed/do not exist
-  if (url.substring(19,30).valueOf() === filter || url.substring(0,10).valueOf() === imageShackFilter) {
-    return false;
+  // filters out images that are removed/do not exist
+  if (url.substring(19, 30).valueOf() === filter || url.substring(0, 10).valueOf() === imageShackFilter) {
+    return false
   } else {
-    return true;
+    return true
   }
 }
 
-//Uses headless webkit Phantom to load the entire non-static page
-function parseUrl() {
-  var randURL = generateRandomURL();
-  phantom.create().then(function(ph) {
-      ph.createPage().then(function(page) {
-        page.open(randURL).then(function(status) {
-          page.property('content').then(function(content) {
-            //cheerio parsing html once page is loaded
-            var $ = cheerio.load(content);
-            var img = $("#screenshot-image").attr("src");
-            //filtering image
-            if (filterURL(img)) {
-              console.log(img);
-            } else {
-              console.log("Image not found. Please wait while next url is parsed.. \n");
-            }
-            ph.exit();
-            parseUrl();
-          });
-        });
-      });
-  });
+// Uses headless webkit Phantom to load the entire non-static page
+function parseUrl () {
+  var randURL = generateRandomURL()
+  phantom.create().then(function (ph) {
+    ph.createPage().then(function (page) {
+      page.open(randURL).then(function (status) {
+        page.property('content').then(function (content) {
+          // cheerio parsing html once page is loaded
+          var $ = cheerio.load(content)
+          var img = $('#screenshot-image').attr('src')
+          // filtering image
+          if (filterURL(img)) {
+            console.log(img)
+          } else {
+            console.log('Image not found. Please wait while next url is parsed.. \n')
+          }
+          ph.exit()
+          parseUrl()
+        })
+      })
+    })
+  })
 }
 
-console.log("Starting scraper, please wait while page is being loaded. (Ctrl+C to terminate)");
-parseUrl();
+console.log('Starting scraper, please wait while page is being loaded. (Ctrl+C to terminate)')
+parseUrl()
