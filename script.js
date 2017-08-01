5304cf06cb71e5cea8f37ff6a8c080a19fba9663
Brandon Manke <me@brandonmanke.com>
2017-07-31 19:22:27 -0500

    Added chromeless library implementation
    Only package dependency needed is now just Chromeless, which uses the chrome-remote-interface to run a headless instance of the chrome canary browser.

--- script.js
+++ script.js
@@ -1,64 +1,93 @@
-// Name: Brandon Manke
-// Thanks to https://github.com/bnned
-// Web Scraper using node, cheerio, and phantom to scrape prnt.sc
-// Licenses: Cheerio (MIT), Phantom (ISC)
+/**
+ * @author Brandon Manke
+ * @license MIT
+ *
+ * This webscraper headless chrome instance to pull images from the screenshot site prnt.sc
+ * It generates a random url then pulls an image if it exists at that url. Images may be NSFW.
+ */
+const { Chromeless } = require('chromeless');
 
-var phantom = require('phantom');
-var cheerio = require('cheerio');
+/**
+ * Generates a random url for prnt.sc domain, 5 to 7 characters in length past the '/'
+ * @return {string} randomly generated url
+ */
+function generateRandomURL() {
+  let url = 'http://prnt.sc/';
+  const randomURL = '0123456789abcdefghijklmnopqrstuvwxyz';
 
-// Generates a random prnt.sc url
-function generateRandomURL () {
-  var url = 'http://prnt.sc/';
-  var randomURL = '0123456789abcdefghijklmnopqrstuvwxyz';
-
-  // from 0 to 5-6 characters in length
-  for (var i = 0; i < Math.floor(Math.random() * (6 - 5 + 1)) + 5; i++) {
+  // from 0 to 5-7 characters in length
+  for (let i = 0; i < Math.floor(Math.random() * (7 - 5 + 1)) + 5; i++) {
     url += randomURL.charAt(Math.floor(Math.random() * randomURL.length));
   }
   return url;
 }
 
 /**
-* @param {string} url that is passed into the filter. In this case it is always passed by generateRandomURL()
-*/
-function filterURL (url) {
-  var filter = '8tdUI8N.png';
-  var imageShackFilter = 'http://img'; // might need to specify this more or change it
+ * Filters url that scraper pulls from page.
+ * @param {string} url that is passed into the filter. In this case it is always passed by generateRandomURL()
+ * @return {boolean} false if image does not exist
+ * @return {boolean} true if image exists
+ */
+function isValidURL(url) {
+  const filter = '8tdUI8N.png';
+  const newFilter = 'https://st.prntscr.com/2017/07/03/0920/img/0_173a7b_211be8ff.png';
+  const imageShackFilter = 'http://img'; // might need to specify this more or change it
 
   // filters out images that are removed/do not exist
-  if (url.substring(19, 30).valueOf() === filter || url.substring(0, 10).valueOf() === imageShackFilter) {
+  if ((url.substring(19, 30).valueOf() === filter) 
+    || url.substring(0, 10).valueOf() === imageShackFilter
+    || url === newFilter
+    || url === '') {
     return false;
   } else {
     return true;
   }
 }
 
-// Uses headless webkit Phantom to load the entire non-static page
-function parseUrl () {
-  /** @param {object} ph - creating phantom object ph */
-  phantom.create().then(function (ph) {
-    /** @param {object} page - creating page to proxy all functions sent to phantom object */
-    ph.createPage().then(function (page) {
-      // pass random url to page
-      page.open(generateRandomURL()).then(function (status) {
-        // loads all content of the page via 'content' property
-        page.property('content').then(function (content) {
-          // cheerio parsing html once page is loaded
-          var $ = cheerio.load(content);
-          var img = $('#screenshot-image').attr('src'); // looks for 'screenshot-image'
-          // filtering image
-          if (filterURL(img)) {
-            console.log(img);
-          } else {
-            console.log('Image not found. Please wait while next url is parsed.. \n');
-          }
-          ph.exit(); // Exiting webkit process
-          parseUrl();
-        });
-      });
+/**
+ * Using the chromeless to visit random page and find the users screen shot src
+ * @return {string} url of image if it passes the url filter
+ * @return {null} null if image does not bypass filter (i.e. doesn't exist or was removed)
+ */
+async function scraper() {
+  const chromeless = new Chromeless();
+  const randomURL = generateRandomURL();
+
+  const randomImage = await chromeless
+    .goto(randomURL)
+    .wait('#screenshot-image')
+    .evaluate(() => {
+      const img = document.querySelector('#screenshot-image').src;
+      return img;
     });
-  });
+
+  console.log('image:', randomImage);
+  await chromeless.end();
+
+  if (isValidURL(randomImage)) {
+    return randomImage;
+  } else {
+    return null;
+  }
 }
 
-console.log('Starting scraper, please wait while page is being loaded. (Ctrl+C to terminate)');
-parseUrl();
+/**
+ * Our main asynchronous runtime logic. It will run until the scraper finds an image that exists. 
+ * (i.e. When scraper() doesn't return null)
+ */
+async function run() {
+  try {
+    const result = await scraper();
+    if (!result) {
+      run();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// canary --remote-debugging-port=9222 --disable-gpu --headless
+// https://github.com/graphcool/chromeless/tree/master/serverless
+// https://martinfowler.com/articles/serverless.html
+// http://csapp.cs.cmu.edu/
+run();
